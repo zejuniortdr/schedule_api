bc7919fe6f7f2c82567aa80f00d463aefff0adb4
Jose Luis da Cruz Junior <zejuniortdr@gmail.com>
2024-10-26 23:25:44 -0300

    docs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const eventRoutes = require('./src/events/routes');
+const eventRoutes = require('./src/routes/event');
+const setupSwagger = require('./swagger');
 
 const app = express();
 const port = 3000;
@@ -12,9 +13,16 @@ mongoose.connect('mongodb://mongodb:27017/events')
 .then(() => console.log('Conectado ao MongoDB'))
 .catch(err => console.error('Erro ao conectar ao MongoDB', err));
 
+
 // Usa as rotas de eventos
 app.use('/events', eventRoutes);
 
+
+// Configuração do Swagger
+setupSwagger(app);
+
+
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Documentação da API em http://localhost:${port}/docs`);
 });
